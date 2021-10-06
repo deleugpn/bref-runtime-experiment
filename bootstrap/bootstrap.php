51d66ca8b1bcefdab69145216038f5b4567f54f1
Deleu <deleugyn@gmail.com>
2021-10-06 20:31:08 +0200

    Testing bootstrap

---
+++ bootstrap/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$lambdaRuntime = \Bref\Runtime\LambdaRuntime::fromEnvironmentVariable('remi-collect-php-experiment');
+
+$function = require getenv('_HANDLER');
+
+$loopMax = 10;
+$loops = 0;
+
+while (true) {
+    if (++$loops > $loopMax) {
+        exit(0);
+    }
+    $success = $lambdaRuntime->processNextEvent($function);
+    // In case the execution failed, we force starting a new process regardless of BREF_LOOP_MAX
+    // Why: an exception could have left the application in a non-clean state, this is preventive
+    if (! $success) {
+        exit(0);
+    }
+}
